ccceb33ba5b47e601cedbf9d3e3b048b9342230f
ChandrakantPal <sanjay161097@gmail.com>
2021-05-20 22:43:04 +0530

    index.js to index.tsx

---
+++ pages/index.tsx
@@ -0,0 +1,13 @@
+import Head from 'next/head'
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>CK16DEV</title>
+        <meta name="description" content="Chandrakant Pal frontend developer" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+    </div>
+  )
+}
