a072eb81d4a283912d230563db8934b949181fdc
ChandrakantPal <sanjay161097@gmail.com>
2021-05-22 20:06:39 +0530

    logo component

---
+++ components/Logo.tsx
@@ -0,0 +1,10 @@
+const Logo = () => {
+  return (
+    <div className="flex justify-center">
+      <h1 className="text-4xl font-semibold">ck16 {'>'}_</h1>
+      <span className="ml-4 border-l-8 border-green-700 border-solid max-h-9 animate-pulse" />
+    </div>
+  )
+}
+
+export default Logo
