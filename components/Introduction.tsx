2917ebb58133846d13513023acdffba5e633666a
ChandrakantPal <sanjay161097@gmail.com>
2021-05-24 20:28:40 +0530

    introduction created

---
+++ components/Introduction.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Introduction = () => {
+  return (
+    <section className="flex flex-col items-center justify-center w-full mx-auto h-96">
+      <p className="text-lg text-left">Hi,</p>
+      <p className="text-xl text-left">I am</p>
+      <p className="text-3xl text-left">Chandrakant Pal</p>
+      <p className="text-3xl text-left">I build things on the web</p>
+    </section>
+  )
+}
+
+export default Introduction
