695fedf726786f040a5bf773d43ab4f93e6d6378
ChandrakantPal <sanjay161097@gmail.com>
2021-05-24 20:01:42 +0530

    logo color changed

--- components/Logo.tsx
+++ components/Logo.tsx
@@ -1,8 +1,8 @@
 const Logo = () => {
   return (
     <div className="flex justify-center">
-      <h1 className="text-4xl font-semibold">ck16 {'>'}_</h1>
-      <span className="ml-4 border-l-8 border-green-700 border-solid max-h-9 animate-pulse" />
+      <h1 className="text-4xl font-semibold text-green-700">ck16 {'>'}_</h1>
+      <span className="ml-4 border-l-8 border-solid max-h-9 animate-pulse" />
     </div>
   )
 }
