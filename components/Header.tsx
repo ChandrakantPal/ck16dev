8ff68dd4dca7231d13c1a85f83238d4cf576c045
ChandrakantPal <sanjay161097@gmail.com>
2021-05-25 23:49:55 +0530

    header fixed

--- components/Header.tsx
+++ components/Header.tsx
@@ -3,7 +3,7 @@ import Logo from './Logo'
 
 const Header = () => {
   return (
-    <header className="flex flex-col flex-wrap items-start p-4 md:justify-between sm:flex-row sm:justify-center">
+    <header className="fixed top-0 left-0 right-0 z-10 flex flex-col flex-wrap items-start h-auto p-4 md:justify-between sm:flex-row sm:justify-center">
       <Logo />
       <div className="w-full my-4 border-t border-dashed md:hidden" />
       <div className="flex items-center justify-center my-2 md:m-0">
