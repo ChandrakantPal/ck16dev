beab718b46453275d26ece106bb3a9656fe7f7bc
ChandrakantPal <sanjay161097@gmail.com>
2021-05-25 22:50:30 +0530

    typing css

--- components/Introduction.tsx
+++ components/Introduction.tsx
@@ -3,10 +3,12 @@ import React from 'react'
 const Introduction = () => {
   return (
     <section className="flex flex-col items-center justify-center w-full mx-auto h-96">
-      <p className="text-lg text-left">Hi,</p>
-      <p className="text-xl text-left">I am</p>
-      <p className="text-3xl text-left">Chandrakant Pal</p>
-      <p className="text-3xl text-left">I build things on the web</p>
+      <div className="text-left whitespace-nowrap typing">
+        <p className="mt-1 text-2xl">Hi,</p>
+        <p className="mt-1 text-3xl">I am</p>
+        <p className="mt-1 text-5xl">Chandrakant Pal</p>
+        <p className="mt-1 text-5xl">I build things on the web</p>
+      </div>
     </section>
   )
 }
