d3ba9d6ac84e3b0f6090040cee235bbbcf1cefb6
ChandrakantPal <sanjay161097@gmail.com>
2021-05-25 23:50:27 +0530

    responsive intro

--- components/Introduction.tsx
+++ components/Introduction.tsx
@@ -2,19 +2,19 @@ import Typist from 'react-typist'
 
 const Introduction = () => {
   return (
-    <section className="flex flex-col items-center justify-center w-full mx-auto h-96">
+    <section className="flex flex-col items-center justify-center w-full h-screen mx-auto">
       <div className="text-left whitespace-nowrap">
         <Typist cursor={{ show: false }}>
-          <p className="mt-1 text-4xl">Hi,</p>
+          <p className="mt-1 text-lg md:text-4xl">Hi,</p>
         </Typist>
         <Typist cursor={{ show: false }}>
           <Typist.Delay ms={1000} />
-          <p className="mt-1 text-4xl">I am</p>
-          <p className="mt-1 text-6xl">Chandrakant Pal</p>
+          <p className="mt-1 text-lg md:text-4xl">I am</p>
+          <p className="mt-1 text-xl md:text-7xl">Chandrakant Pal</p>
         </Typist>
         <Typist cursor={{ show: false }}>
           <Typist.Delay ms={2500} />
-          <p className="mt-1 text-6xl">I build things on the web.</p>
+          <p className="mt-1 text-xl md:text-7xl">I build things on the web.</p>
         </Typist>
       </div>
     </section>
