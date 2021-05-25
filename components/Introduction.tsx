f178ae9f0e7f1cf77cc489daa56ca5d8ea444ade
ChandrakantPal <sanjay161097@gmail.com>
2021-05-25 23:42:16 +0530

    typing effect for into

--- components/Introduction.tsx
+++ components/Introduction.tsx
@@ -1,13 +1,21 @@
-import React from 'react'
+import Typist from 'react-typist'
 
 const Introduction = () => {
   return (
     <section className="flex flex-col items-center justify-center w-full mx-auto h-96">
       <div className="text-left whitespace-nowrap">
-        <p className="mt-1 text-2xl">Hi,</p>
-        <p className="mt-1 text-3xl">I am</p>
-        <p className="mt-1 text-5xl">Chandrakant Pal</p>
-        <p className="mt-1 text-5xl">I build things on the web</p>
+        <Typist cursor={{ show: false }}>
+          <p className="mt-1 text-4xl">Hi,</p>
+        </Typist>
+        <Typist cursor={{ show: false }}>
+          <Typist.Delay ms={1000} />
+          <p className="mt-1 text-4xl">I am</p>
+          <p className="mt-1 text-6xl">Chandrakant Pal</p>
+        </Typist>
+        <Typist cursor={{ show: false }}>
+          <Typist.Delay ms={2500} />
+          <p className="mt-1 text-6xl">I build things on the web.</p>
+        </Typist>
       </div>
     </section>
   )
