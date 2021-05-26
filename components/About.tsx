e0b53f6dc35af97a4a81daa54ae3a9c20b00f580
ChandrakantPal <sanjay161097@gmail.com>
2021-05-26 22:23:55 +0530

    about section created

---
+++ components/About.tsx
@@ -0,0 +1,13 @@
+const About = () => {
+  return (
+    <>
+      <h1>/about</h1>
+      <section className="flex flex-col items-center justify-center w-full h-screen mx-auto">
+        <div>Desription</div>
+        <div>Picture</div>
+      </section>
+    </>
+  )
+}
+
+export default About
