973dfff88471e0f903993d87e1c564b58a86cf3b
ChandrakantPal <sanjay161097@gmail.com>
2021-05-29 22:49:52 +0530

    work section

---
+++ components/Work.tsx
@@ -0,0 +1,13 @@
+const Work = () => {
+  return (
+    <>
+      <h1>/work</h1>
+      <section className="flex flex-col items-center justify-center w-full h-screen mx-auto">
+        <div>Desription</div>
+        <div>Picture</div>
+      </section>
+    </>
+  )
+}
+
+export default Work
