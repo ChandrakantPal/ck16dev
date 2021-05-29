024793d812f59a8021027a2c3fff9e4357a4b225
ChandrakantPal <sanjay161097@gmail.com>
2021-05-29 22:50:56 +0530

    container and padding added

--- components/Introduction.tsx
+++ components/Introduction.tsx
@@ -1,9 +1,10 @@
 import Typist from 'react-typist'
+import Image from 'next/image'
 
 const Introduction = () => {
   return (
-    <section className="flex flex-col items-center justify-center w-full h-screen mx-auto">
-      <div className="text-left whitespace-nowrap">
+    <section className="flex flex-col items-center justify-center mt-14">
+      <div className="px-20 py-20 text-left">
         <Typist cursor={{ show: false }}>
           <p className="mt-1 text-lg md:text-4xl">Hi,</p>
         </Typist>
@@ -15,12 +16,14 @@ const Introduction = () => {
         <Typist cursor={{ show: false }}>
           <Typist.Delay ms={2500} />
           <p className="mt-1 text-xl md:text-6xl">I build things on the web.</p>
-          <p className="mt-2 text-xl">
-            I am software engineer based out Goa, India. <br /> Tech enthusiast,
-            constantly learning some new tech out there.
-            <br /> History buff,
-            <br /> Movie buff.
-          </p>
+          <div className="max-w-sm md:w-full">
+            <p className="mt-2 text-xs md:text-xl">
+              I am software engineer based out Goa, India. <br /> Tech
+              enthusiast, constantly learning some new tech out there.
+              <br /> History buff,
+              <br /> Movie buff.
+            </p>
+          </div>
         </Typist>
       </div>
     </section>
