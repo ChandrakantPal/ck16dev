871eee475c8b40feb3ef50e3503dd9f3b3096992
ChandrakantPal <sanjay161097@gmail.com>
2021-05-29 23:45:50 +0530

    animate slide

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,19 @@ module.exports = {
       fontFamily: {
         body: ['Roboto Mono'],
       },
+      animation: {
+        slide: 'slide 1s',
+      },
+      keyframes: {
+        slide: {
+          '0%': {
+            transform: 'translateY(0)',
+          },
+          '100%': {
+            transform: 'translateY(10px)',
+          },
+        },
+      },
     },
   },
   variants: {
