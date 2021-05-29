f500a6ed201364b301b4bdad6819b066b370e362
ChandrakantPal <sanjay161097@gmail.com>
2021-05-30 00:04:37 +0530

    width height padding and margin changes

--- components/Introduction.tsx
+++ components/Introduction.tsx
@@ -3,21 +3,23 @@ import Image from 'next/image'
 
 const Introduction = () => {
   return (
-    <section className="flex flex-col items-center justify-center h-screen">
-      <div className="px-20 py-20 text-left">
+    <section className="flex flex-col items-center justify-center w-full h-screen">
+      <div className="w-full h-full px-10 py-6 mt-20 text-left md:px-20 md:mt-52">
         <Typist cursor={{ show: false }}>
-          <p className="mt-1 text-lg md:text-4xl">Hi,</p>
+          <p className="mt-1 text-xl md:text-4xl">Hi,</p>
         </Typist>
         <Typist cursor={{ show: false }}>
           <Typist.Delay ms={1000} />
-          <p className="mt-1 text-lg md:text-4xl">I am</p>
-          <p className="mt-1 text-xl md:text-6xl">Chandrakant Pal</p>
+          <p className="mt-1 text-xl md:text-4xl">I am</p>
+          <p className="mt-1 text-2xl md:text-6xl">Chandrakant Pal</p>
         </Typist>
         <Typist cursor={{ show: false }}>
           <Typist.Delay ms={2500} />
-          <p className="mt-1 text-xl md:text-6xl">I build things on the web.</p>
-          <div className="max-w-sm md:w-full">
-            <p className="mt-2 text-xs md:text-xl">
+          <p className="mt-1 text-2xl md:text-6xl">
+            I build things on the web.
+          </p>
+          <div className="w-full">
+            <p className="mt-2 text-lg md:text-2xl">
               I am software engineer based out Goa, India. <br /> Tech
               enthusiast, constantly learning some new tech out there.
               <br /> History buff,
