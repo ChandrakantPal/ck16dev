70ca7a29e2893ce671c8b374f1a4e246ffe7806d
ChandrakantPal <sanjay161097@gmail.com>
2021-05-29 22:50:26 +0530

    skills section

---
+++ components/Skills.tsx
@@ -0,0 +1,13 @@
+const Skills = () => {
+  return (
+    <>
+      <h1>/skills</h1>
+      <section className="flex flex-col items-center justify-center w-full h-screen mx-auto">
+        <div>Desription</div>
+        <div>Picture</div>
+      </section>
+    </>
+  )
+}
+
+export default Skills
