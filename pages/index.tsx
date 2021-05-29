9586a34f1169d938168f65e60036bcafca65eabc
ChandrakantPal <sanjay161097@gmail.com>
2021-05-29 22:51:46 +0530

    side drawer state

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,15 @@
 import Head from 'next/head'
+import { useState } from 'react'
 import About from '../components/About'
+import Contact from '../components/Contact'
 import Header from '../components/Header'
 import Introduction from '../components/Introduction'
+import SideDrawer from '../components/SideDrawer'
+import Skills from '../components/Skills'
+import Work from '../components/Work'
 
 export default function Home() {
+  const [menuToggle, setMenuToggle] = useState(false)
   return (
     <div>
       <Head>
@@ -11,9 +17,15 @@ export default function Home() {
         <meta name="description" content="Chandrakant Pal frontend developer" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <Header />
-      <Introduction />
-      <About />
+      <Header toggle={menuToggle} setToggle={setMenuToggle} />
+      {menuToggle && <SideDrawer setToggle={setMenuToggle} />}
+      <main className="container mx-auto">
+        <Introduction />
+        <About />
+        <Skills />
+        <Work />
+        <Contact />
+      </main>
     </div>
   )
 }
