cb0c346405f4be582c3e9cd49404fe017b056b84
ChandrakantPal <sanjay161097@gmail.com>
2021-05-30 22:04:38 +0530

    animation duration changed

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  // mode: 'jit',
+  mode: 'jit',
   purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
   darkMode: false, // or 'media' or 'class'
   theme: {
@@ -8,7 +8,7 @@ module.exports = {
         body: ['Roboto Mono'],
       },
       animation: {
-        slide: 'slide 1s',
+        slide: 'slide 0.3s',
       },
       keyframes: {
         slide: {
