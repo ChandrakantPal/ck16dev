e889bcff2855d7109502732f9741c617e74926cd
ChandrakantPal <sanjay161097@gmail.com>
2021-05-30 23:32:14 +0530

    about section content

--- components/About.tsx
+++ components/About.tsx
@@ -1,12 +1,32 @@
 const About = () => {
   return (
-    <>
-      <h1>/about</h1>
-      <section className="flex flex-col items-center justify-center w-full h-screen mx-auto">
-        <div>Desription</div>
-        <div>Picture</div>
-      </section>
-    </>
+    <section className="w-full h-screen">
+      <h1 className="text-2xl text-left md:text-4xl">./about</h1>
+      <hr className="my-2 border-green-700" />
+      <div className="flex flex-wrap-reverse items-start justify-center mt-10 md:flex-nowrap">
+        <div className="w-full mx-10 my-5 md:px-20 md:w-1/2">
+          <p className="text-gray-500 md:text-2xl">
+            It all started in 3rd year of college where my friend{' '}
+            <a
+              href="https://www.linkedin.com/in/gomesluis479/"
+              className="text-green-700"
+            >
+              {'('}Luis{')'}
+            </a>{' '}
+            introduced me to web developement, and our finial year project also
+            need us to make a web dashboard, so thats where it all began and now
+            I really enjoy builing Products. I initially started as a frontend
+            dev and now I do both frontend and backend.
+          </p>
+        </div>
+        <div className="w-full mx-24 md:mx-10 md:w-1/2">
+          <img
+            src="/images/me.jpg"
+            className="object-contain rounded-lg shadow-xl w-44 md:w-56 filter saturate-0"
+          />
+        </div>
+      </div>
+    </section>
   )
 }
 
