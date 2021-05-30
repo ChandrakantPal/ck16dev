bd437843f990a88fb6af58e3d6def44b8a959913
ChandrakantPal <sanjay161097@gmail.com>
2021-05-30 22:06:04 +0530

    intro text color changed

--- components/Introduction.tsx
+++ components/Introduction.tsx
@@ -6,21 +6,23 @@ const Introduction = () => {
     <section className="flex flex-col items-center justify-center w-full h-screen">
       <div className="w-full h-full px-10 py-6 mt-20 text-left md:px-20 md:mt-52">
         <Typist cursor={{ show: false }}>
-          <p className="mt-1 text-xl md:text-4xl">Hi,</p>
+          <p className="mt-1 text-xl text-green-400 md:text-4xl">Hi, I am</p>
         </Typist>
         <Typist cursor={{ show: false }}>
           <Typist.Delay ms={1000} />
-          <p className="mt-1 text-xl md:text-4xl">I am</p>
-          <p className="mt-1 text-2xl md:text-6xl">Chandrakant Pal</p>
+          {/* <p className="mt-1 text-xl text-green-400 md:text-4xl"></p> */}
+          <p className="mt-1 text-2xl text-gray-400 md:text-6xl">
+            Chandrakant Pal
+          </p>
         </Typist>
         <Typist cursor={{ show: false }}>
           <Typist.Delay ms={2500} />
-          <p className="mt-1 text-2xl md:text-6xl">
+          <p className="mt-1 text-2xl text-gray-500 md:text-6xl">
             I build things on the web.
           </p>
           <div className="w-full">
-            <p className="mt-2 text-lg md:text-2xl">
-              I am software engineer based out Goa, India. <br /> Tech
+            <p className="mt-2 text-lg text-gray-500 md:text-2xl">
+              I am software engineer based out of Goa, India. <br /> Tech
               enthusiast, constantly learning some new tech out there.
               <br /> History buff,
               <br /> Movie buff.
