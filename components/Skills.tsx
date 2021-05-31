60c5dc5f5ca5be8086a88191e510e20a9ff55b3d
ChandrakantPal <sanjay161097@gmail.com>
2021-05-31 23:31:46 +0530

    skills content added

--- components/Skills.tsx
+++ components/Skills.tsx
@@ -1,12 +1,16 @@
 const Skills = () => {
   return (
-    <>
-      <h1>/skills</h1>
-      <section className="flex flex-col items-center justify-center w-full h-screen mx-auto">
-        <div>Desription</div>
-        <div>Picture</div>
+    <section className="w-full h-screen">
+      <h1 className="text-2xl text-left md:text-4xl">.//skills</h1>
+      <hr className="my-2 border-green-700" />
+      <section className="w-auto mx-auto">
+        <div>ReactJS '['****************....']'and learning</div>
+        <div>NextJS '['****************....']'and learning</div>
+        <div>Typescript '['****************....']'and learning</div>
+        <div>NodeJS '['****************....']'and learning</div>
+        <div>PostgreSQL '['**********..........']'and learning</div>
       </section>
-    </>
+    </section>
   )
 }
 
