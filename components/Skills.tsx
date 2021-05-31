840ed5140fb71204bc37e80d32a63aa04d0fb69f
ChandrakantPal <sanjay161097@gmail.com>
2021-05-31 23:37:15 +0530

    text color changed

--- components/Skills.tsx
+++ components/Skills.tsx
@@ -1,14 +1,24 @@
 const Skills = () => {
   return (
     <section className="w-full h-screen">
-      <h1 className="text-2xl text-left md:text-4xl">.//skills</h1>
+      <h1 className="text-2xl text-left md:text-4xl">./skills</h1>
       <hr className="my-2 border-green-700" />
-      <section className="w-auto mx-auto">
-        <div>ReactJS '['****************....']'and learning</div>
-        <div>NextJS '['****************....']'and learning</div>
-        <div>Typescript '['****************....']'and learning</div>
-        <div>NodeJS '['****************....']'and learning</div>
-        <div>PostgreSQL '['**********..........']'and learning</div>
+      <section className="w-full mx-auto md:w-1/2">
+        <p className="py-10 text-2xl text-left text-gray-500">
+          ReactJS '['****************....']'and learning
+        </p>
+        <p className="py-10 text-2xl text-left text-gray-500">
+          NextJS '['****************....']'and learning
+        </p>
+        <p className="py-10 text-2xl text-left text-gray-500">
+          Typescript '['****************....']'and learning
+        </p>
+        <p className="py-10 text-2xl text-left text-gray-500">
+          NodeJS '['****************....']'and learning
+        </p>
+        <p className="py-10 text-2xl text-left text-gray-500">
+          PostgreSQL '['**********..........']'and learning
+        </p>
       </section>
     </section>
   )
