66f7c6ba3409af90db940374262630f61f06e65f
ChandrakantPal <sanjay161097@gmail.com>
2021-06-01 22:35:24 +0530

    skill component created

---
+++ components/Skill.tsx
@@ -0,0 +1,31 @@
+import { FC } from 'react'
+
+interface SkillProp {
+  title: string
+  url: string
+  proficiency: number
+}
+
+const Skill: FC<SkillProp> = ({ title, url, proficiency }) => {
+  const rating = `${'**'.repeat(proficiency)}${'..'.repeat(10 - proficiency)}`
+  return (
+    <>
+      <div className="flex flex-wrap items-start w-full mb-7 md:my-16 md:justify-between">
+        <div className="flex items-center">
+          <div className="w-12 h-12 mr-2 md:w-16 md:h-16">
+            <img
+              src={url}
+              className="object-contain max-w-full filter invert"
+            />
+          </div>
+          <p className="text-gray-500 md:text-2xl">{title}</p>
+        </div>
+        <p className="mt-1 text-gray-500 md:text-2xl md:mt-0">
+          [{rating}] and learning
+        </p>
+      </div>
+    </>
+  )
+}
+
+export default Skill
