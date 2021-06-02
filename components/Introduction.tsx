949f4a59163d97a3c3fe618253229761989ed661
ChandrakantPal <sanjay161097@gmail.com>
2021-06-03 01:04:06 +0530

    responsive for lg screens

--- components/Introduction.tsx
+++ components/Introduction.tsx
@@ -3,24 +3,24 @@ import Image from 'next/image'
 
 const Introduction = () => {
   return (
-    <section className="flex flex-col items-center justify-center w-full h-200">
-      <div className="w-full h-full px-8 py-6 mt-20 text-left md:px-20 md:py-20">
+    <section className="flex flex-col items-center justify-center w-full h-200 lg:h-screen">
+      <div className="w-full h-full p-6 mt-20 text-left md:p-14 lg:p-20">
         <Typist cursor={{ show: false }}>
-          <p className="mt-1 text-2xl text-green-400 md:text-4xl">Hi, I am</p>
+          <p className="mt-1 text-lg text-green-400 md:text-xl">Hi, I am</p>
         </Typist>
         <Typist cursor={{ show: false }}>
           <Typist.Delay ms={1000} />
-          <p className="mt-1 text-3xl text-gray-400 md:text-6xl">
-            Chandrakant Pal
+          <p className="mt-6 text-3xl font-semibold text-gray-400 md:text-4xl lg:text-6xl">
+            Chandrakant Pal.
           </p>
         </Typist>
         <Typist cursor={{ show: false }}>
           <Typist.Delay ms={2500} />
-          <p className="mt-1 text-3xl text-gray-500 md:text-6xl">
+          <p className="mt-1 text-3xl font-semibold text-gray-500 md:text-4xl lg:text-6xl">
             I build things on the web.
           </p>
           <div className="w-full">
-            <p className="mt-2 text-xl text-gray-500 md:text-2xl">
+            <p className="mt-8 text-lg text-gray-500 md:text-xl">
               I am Web Developer based out of Goa, India. <br /> Tech
               enthusiast, constantly learning some new tech out there.
               <br /> History buff,
