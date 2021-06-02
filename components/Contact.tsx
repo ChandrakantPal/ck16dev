e8f19236798a17dfc0fe173cbec4facdd8519d22
ChandrakantPal <sanjay161097@gmail.com>
2021-06-03 01:03:45 +0530

    logo size reduced on mibile

--- components/Contact.tsx
+++ components/Contact.tsx
@@ -7,17 +7,17 @@ const Contact = () => {
       <h1 className="mt-4 text-center text-gray-500 tex-xl md:text-2xl">
         Get in touch with me
       </h1>
-      <div className="flex items-center justify-center h-40">
+      <div className="flex items-center justify-center h-20 md:h-40">
         <a href="https://github.com/ChandrakantPal" target="_blank">
           <img
             src="/images/logos/github.png"
-            className="object-contain w-16 mx-4 filter invert"
+            className="object-contain w-10 mx-4 md:w-16 filter invert"
           />
         </a>
         <a href="https://www.linkedin.com/in/chandrakant-pal" target="_blank">
           <img
             src="/images/logos/linkedin.png"
-            className="object-contain w-16 mx-4"
+            className="object-contain w-10 mx-4 md:w-16"
           />
         </a>
       </div>
