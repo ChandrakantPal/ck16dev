78cb92a4f39449824b7c4b08264a34a5298f54a1
ChandrakantPal <sanjay161097@gmail.com>
2021-06-03 00:24:28 +0530

    contact section content

--- components/Contact.tsx
+++ components/Contact.tsx
@@ -2,8 +2,25 @@ import SectionHeader from './SectionHeader'
 
 const Contact = () => {
   return (
-    <section className="w-full h-200">
+    <section className="w-full h-auto">
       <SectionHeader title="contact" />
+      <h1 className="mt-4 text-center text-gray-500 tex-xl md:text-2xl">
+        Get in touch with me
+      </h1>
+      <div className="flex items-center justify-center h-40">
+        <a href="https://github.com/ChandrakantPal" target="_blank">
+          <img
+            src="/images/logos/github.png"
+            className="object-contain w-16 mx-4 filter invert"
+          />
+        </a>
+        <a href="https://www.linkedin.com/in/chandrakant-pal" target="_blank">
+          <img
+            src="/images/logos/linkedin.png"
+            className="object-contain w-16 mx-4"
+          />
+        </a>
+      </div>
     </section>
   )
 }
