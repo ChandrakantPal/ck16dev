9440db43d726709cdd24fa02e111a331c4403d37
ChandrakantPal <sanjay161097@gmail.com>
2021-06-02 21:47:57 +0530

    height margins and font sizes fixed

--- components/Introduction.tsx
+++ components/Introduction.tsx
@@ -3,26 +3,25 @@ import Image from 'next/image'
 
 const Introduction = () => {
   return (
-    <section className="flex flex-col items-center justify-center w-full h-screen">
-      <div className="w-full h-full px-10 py-6 mt-20 text-left md:px-20 md:mt-52">
+    <section className="flex flex-col items-center justify-center w-full h-200">
+      <div className="w-full h-full px-8 py-6 mt-20 text-left md:px-20 md:py-20">
         <Typist cursor={{ show: false }}>
-          <p className="mt-1 text-xl text-green-400 md:text-4xl">Hi, I am</p>
+          <p className="mt-1 text-2xl text-green-400 md:text-4xl">Hi, I am</p>
         </Typist>
         <Typist cursor={{ show: false }}>
           <Typist.Delay ms={1000} />
-          {/* <p className="mt-1 text-xl text-green-400 md:text-4xl"></p> */}
-          <p className="mt-1 text-2xl text-gray-400 md:text-6xl">
+          <p className="mt-1 text-3xl text-gray-400 md:text-6xl">
             Chandrakant Pal
           </p>
         </Typist>
         <Typist cursor={{ show: false }}>
           <Typist.Delay ms={2500} />
-          <p className="mt-1 text-2xl text-gray-500 md:text-6xl">
+          <p className="mt-1 text-3xl text-gray-500 md:text-6xl">
             I build things on the web.
           </p>
           <div className="w-full">
-            <p className="mt-2 text-lg text-gray-500 md:text-2xl">
-              I am software engineer based out of Goa, India. <br /> Tech
+            <p className="mt-2 text-xl text-gray-500 md:text-2xl">
+              I am Web Developer based out of Goa, India. <br /> Tech
               enthusiast, constantly learning some new tech out there.
               <br /> History buff,
               <br /> Movie buff.
