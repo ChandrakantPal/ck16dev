d5e24ec1f4536592cf352435dfa32fcc8ca8e1d8
ChandrakantPal <sanjay161097@gmail.com>
2021-06-09 23:08:41 +0530

    hello world peoject added

--- components/Work.tsx
+++ components/Work.tsx
@@ -1,9 +1,17 @@
+import Project from './Project'
 import SectionHeader from './SectionHeader'
 
 const Work = () => {
   return (
     <section className="w-full h-200">
       <SectionHeader title="work" />
+      <div className="grid grid-cols-3 gap-5 mt-10">
+        <Project
+          link="https://helloworldtwitterclonegraphql.netlify.app/"
+          title="Hello World (Twitter clone)"
+          stack="React Node MongoDB GraphQL"
+        />
+      </div>
     </section>
   )
 }
