0819431181b41a66655b5be607436af54de443d7
ChandrakantPal <sanjay161097@gmail.com>
2021-07-31 01:45:09 +0530

    fade right

--- components/Skill.tsx
+++ components/Skill.tsx
@@ -1,5 +1,4 @@
-import Aos from 'aos'
-import { FC, useEffect } from 'react'
+import { FC } from 'react'
 
 interface SkillProp {
   title: string
@@ -8,14 +7,11 @@ interface SkillProp {
 }
 
 const Skill: FC<SkillProp> = ({ title, url, proficiency }) => {
-  useEffect(() => {
-    Aos.init()
-  }, [])
   const rating = `${'**'.repeat(proficiency)}${'..'.repeat(10 - proficiency)}`
   return (
     <>
       <div
-        data-aos="fade-up"
+        data-aos="fade-right"
         className="flex flex-wrap items-start w-full mb-7 md:my-16 md:justify-around"
       >
         <div className="flex items-center">
