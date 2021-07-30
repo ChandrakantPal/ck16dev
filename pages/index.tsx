ed68bf68ec3950e1ada7726744b8ca1a39cc3d37
ChandrakantPal <sanjay161097@gmail.com>
2021-07-31 01:46:17 +0530

    props passed

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,5 @@
 import Head from 'next/head'
-import { useEffect, useState } from 'react'
+import { Ref, RefObject, useEffect, useRef, useState } from 'react'
 import aos from 'aos'
 import About from '../components/About'
 import Contact from '../components/Contact'
@@ -10,11 +10,25 @@ import Skills from '../components/Skills'
 import Work from '../components/Work'
 
 export default function Home() {
+  const heroRef = useRef<HTMLElement>(null)
+  const aboutRef = useRef<HTMLElement>(null)
+  const skillsRef = useRef<HTMLElement>(null)
+  const workRef = useRef<HTMLElement>(null)
+  const contactRef = useRef<HTMLElement>(null)
+
+  const scrollToRef = (ref: RefObject<HTMLElement>) => {
+    ref.current.scrollIntoView({
+      behavior: 'smooth',
+      block: 'center',
+    })
+  }
+
   const [menuToggle, setMenuToggle] = useState(false)
   useEffect(() => {
     aos.init({
       offset: 400,
       duration: 1000,
+      once: true,
     })
   }, [])
   return (
@@ -24,14 +38,32 @@ export default function Home() {
         <meta name="description" content="Chandrakant Pal frontend developer" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <Header toggle={menuToggle} setToggle={setMenuToggle} />
-      {menuToggle && <SideDrawer setToggle={setMenuToggle} />}
+      <Header
+        toggle={menuToggle}
+        setToggle={setMenuToggle}
+        scrollToRef={scrollToRef}
+        heroRef={heroRef}
+        aboutRef={aboutRef}
+        skillsRef={skillsRef}
+        workRef={workRef}
+        contactRef={contactRef}
+      />
+      {menuToggle && (
+        <SideDrawer
+          setToggle={setMenuToggle}
+          scrollToRef={scrollToRef}
+          aboutRef={aboutRef}
+          skillsRef={skillsRef}
+          workRef={workRef}
+          contactRef={contactRef}
+        />
+      )}
       <main className="container mx-auto">
-        <Introduction />
-        <About />
-        <Skills />
-        <Work />
-        <Contact />
+        <Introduction heroRef={heroRef} />
+        <About aboutRef={aboutRef} />
+        <Skills skillsRef={skillsRef} />
+        <Work workRef={workRef} />
+        <Contact contactRef={contactRef} />
       </main>
     </div>
   )
