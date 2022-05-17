242d149710060378dade805899fa9ed6495f7860
ChandrakantPal <sanjay161097@gmail.com>
2022-05-17 20:31:21 +0530

    projects config used

--- src/components/Work.tsx
+++ src/components/Work.tsx
@@ -1,4 +1,5 @@
 import { FC, RefObject } from 'react'
+import { Projects } from '../utils/project.config'
 import Project from './Project'
 import SectionHeader from './SectionHeader'
 
@@ -7,31 +8,13 @@ const Work: FC<{ workRef: RefObject<HTMLElement> }> = ({ workRef }) => {
     <section data-aos="fade-up" className="w-full mb-24 md:mb-36" ref={workRef}>
       <SectionHeader title="work" />
       <div className="flex flex-wrap grid-cols-3 grid-rows-2 gap-10 mt-10 md:grid">
-        <Project
-          link="https://helloworldtwitterclonegraphql.netlify.app/"
-          title="Hello World (Twitter clone)"
-          stack="React | Node | MongoDB | GraphQL"
-        />
-        <Project
-          link="https://contactsbookclient.netlify.app"
-          title="Contacts Book"
-          stack="React | TypeScript | Tailwind | Node | MongoDB | GraphQL"
-        />
-        <Project
-          link="https://workfloy-clone.netlify.app"
-          title="WorkFlowy Clone"
-          stack="React | TypeScript | Tailwind | Node | MongoDB | GraphQL"
-        />
-        <Project
-          link="https://netflixclonereact.netlify.app"
-          title="Netflix Clone"
-          stack="React"
-        />
-        <Project
-          link="https://hulu-clone-next-typescript-tailwind.vercel.app/"
-          title="Hulu Clone"
-          stack="Next | TypeScript | Tailwind"
-        />
+        {Projects.map((project) => (
+          <Project
+            link={project.link}
+            title={project.title}
+            stack={project.stack}
+          />
+        ))}
       </div>
     </section>
   )
