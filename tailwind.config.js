6b66db856ea9ad6f7eff1fe56133b77d09b9280e
ChandrakantPal <sanjay161097@gmail.com>
2022-05-17 20:49:35 +0530

    routes changed

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,10 @@
 module.exports = {
   // mode: 'jit',
-  purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
-  darkMode: false, // or 'media' or 'class'
+  purge: [
+    './src/pages/**/*.{js,ts,jsx,tsx}',
+    './src/components/**/*.{js,ts,jsx,tsx}',
+  ],
+  // darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {
       fontFamily: {
