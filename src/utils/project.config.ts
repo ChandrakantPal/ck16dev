88975475bc84f21d4461eb45129c74ae75671230
ChandrakantPal <sanjay161097@gmail.com>
2022-05-17 20:24:35 +0530

    order resuffled

--- src/utils/project.config.ts
+++ src/utils/project.config.ts
@@ -1,17 +1,17 @@
 export const Projects = [
   {
-    link: 'https://helloworldtwitterclonegraphql.netlify.app/',
-    title: 'Hello World (Twitter clone)',
-    stack: 'React | Node | MongoDB | GraphQL',
-  },
-  {
-    link: 'https://contactsbookclient.netlify.app',
-    title: 'Contacts Book',
-    stack: 'React | TypeScript | Tailwind | Node | MongoDB | GraphQL',
+    link: 'https://hulu-clone-next-typescript-tailwind.vercel.app/',
+    title: 'Hulu Clone',
+    stack: 'Next | TypeScript | Tailwind',
   },
   {
     link: 'https://workfloy-clone.netlify.app',
     title: 'WorkFlowy Clone',
+    stack: 'React | TypeScript | Tailwind | Node | MongoDB | GraphQL',
+  },
+  {
+    link: 'https://contactsbookclient.netlify.app',
+    title: 'Contacts Book',
     stack: 'React | TypeScript | Tailwind | Node | MongoDB | GraphQL',
   },
   {
@@ -20,8 +20,8 @@ export const Projects = [
     stack: 'React',
   },
   {
-    link: 'https://hulu-clone-next-typescript-tailwind.vercel.app/',
-    title: 'Hulu Clone',
-    stack: 'Next | TypeScript | Tailwind',
+    link: 'https://helloworldtwitterclonegraphql.netlify.app/',
+    title: 'Hello World (Twitter clone)',
+    stack: 'React | Node | MongoDB | GraphQL',
   },
 ]
