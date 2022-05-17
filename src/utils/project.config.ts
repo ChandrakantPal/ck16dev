77854d9f01802c3208b5a64cd3ce87668b778782
ChandrakantPal <sanjay161097@gmail.com>
2022-05-17 20:23:00 +0530

    projects config file

---
+++ src/utils/project.config.ts
@@ -0,0 +1,27 @@
+export const Projects = [
+  {
+    link: 'https://helloworldtwitterclonegraphql.netlify.app/',
+    title: 'Hello World (Twitter clone)',
+    stack: 'React | Node | MongoDB | GraphQL',
+  },
+  {
+    link: 'https://contactsbookclient.netlify.app',
+    title: 'Contacts Book',
+    stack: 'React | TypeScript | Tailwind | Node | MongoDB | GraphQL',
+  },
+  {
+    link: 'https://workfloy-clone.netlify.app',
+    title: 'WorkFlowy Clone',
+    stack: 'React | TypeScript | Tailwind | Node | MongoDB | GraphQL',
+  },
+  {
+    link: 'https://netflixclonereact.netlify.app',
+    title: 'Netflix Clone',
+    stack: 'React',
+  },
+  {
+    link: 'https://hulu-clone-next-typescript-tailwind.vercel.app/',
+    title: 'Hulu Clone',
+    stack: 'Next | TypeScript | Tailwind',
+  },
+]
