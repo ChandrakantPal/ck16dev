0a2e5c3800aabd7fd4d0549f521d8a72bfd463c5
Chandrakant Pal <sanjay161097@gmail.com>
2023-03-21 00:22:29 +0530

    purge changed to content

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,44 +1,44 @@
 module.exports = {
   // mode: 'jit',
-  purge: [
-    './src/pages/**/*.{js,ts,jsx,tsx}',
-    './src/components/**/*.{js,ts,jsx,tsx}',
+  content: [
+    "./src/pages/**/*.{js,ts,jsx,tsx}",
+    "./src/components/**/*.{js,ts,jsx,tsx}",
   ],
   // darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {
       fontFamily: {
-        body: ['Roboto Mono'],
+        body: ["Roboto Mono"],
       },
       animation: {
-        slide: 'slide 0.3s',
+        slide: "slide 0.3s",
       },
       keyframes: {
         slide: {
-          '0%': {
-            transform: 'translateY(0)',
+          "0%": {
+            transform: "translateY(0)",
           },
-          '100%': {
-            transform: 'translateY(10px)',
+          "100%": {
+            transform: "translateY(10px)",
           },
         },
       },
       spacing: {
-        200: '60rem',
+        200: "60rem",
       },
       colors: {
         bunker: {
-          DEFAULT: '#0D1117',
-          50: '#637EA7',
-          100: '#577199',
-          200: '#445979',
-          300: '#324158',
-          400: '#1F2938',
-          500: '#0D1117',
-          600: '#000000',
-          700: '#000000',
-          800: '#000000',
-          900: '#000000',
+          DEFAULT: "#0D1117",
+          50: "#637EA7",
+          100: "#577199",
+          200: "#445979",
+          300: "#324158",
+          400: "#1F2938",
+          500: "#0D1117",
+          600: "#000000",
+          700: "#000000",
+          800: "#000000",
+          900: "#000000",
         },
       },
       container: false,
@@ -50,15 +50,15 @@ module.exports = {
   plugins: [
     function ({ addComponents }) {
       addComponents({
-        '.container': {
-          width: '100%',
-          marginLeft: 'auto',
-          marginRight: 'auto',
-          '@screen sm': { maxWidth: '640px' },
-          '@screen md': { maxWidth: '768px' },
-          '@screen lg': { maxWidth: '975px' },
+        ".container": {
+          width: "100%",
+          marginLeft: "auto",
+          marginRight: "auto",
+          "@screen sm": { maxWidth: "640px" },
+          "@screen md": { maxWidth: "768px" },
+          "@screen lg": { maxWidth: "975px" },
         },
-      })
+      });
     },
   ],
-}
+};
