146956db0064220f90d14ccd8261a59f974f1588
Chandrakant Pal <sanjay161097@gmail.com>
2023-03-22 00:19:35 +0530

    cleanup

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,5 @@
 import Head from "next/head";
-import { Ref, RefObject, useEffect, useRef, useState } from "react";
+import { RefObject, useEffect, useRef, useState } from "react";
 import aos from "aos";
 import About from "../components/About";
 import Contact from "../components/Contact";
@@ -7,13 +7,11 @@ import Header from "../components/Header";
 import Introduction from "../components/Introduction";
 import SideDrawer from "../components/SideDrawer";
 import Skills from "../components/Skills";
-import Work from "../components/Work";
 
 export default function Home() {
   const heroRef = useRef<HTMLElement>(null);
   const aboutRef = useRef<HTMLElement>(null);
   const skillsRef = useRef<HTMLElement>(null);
-  const workRef = useRef<HTMLElement>(null);
   const contactRef = useRef<HTMLElement>(null);
 
   const scrollToRef = (ref: RefObject<HTMLElement>) => {
